8a2eae51a8a83750972524b2ce9790e7f22ed624
Brendan Murphy <bmurphy5216@gmail.com>
2025-08-11 10:49:39 -0400

    Completed ex_6_2.

---
+++ ex_6_2.py
@@ -0,0 +1,8 @@
+deaths = {"Venezuela" : 1830,
+          "El Salvador": 1704,
+            "Syria": 819,
+            "Philippines": 557,
+            "Nicaragua": 523}
+
+for key, value in deaths.items():
+    print(f"The rate of killings by law enforcement officers in {key} is {value}")
